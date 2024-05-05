a0372fe7f34f8c0e553fed7f5b4a292dd57a57b1
Bill <bill.kimtai@student.moringaschool.com>
2024-05-05 15:51:25 +0300

    completed

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,21 @@
-// Your tests here
+import {pointsForWord,isPalindrome} from "../utils"
+
+describe("PointsForWord",()=>{
+    it("calculates the total points for a word (1 point per vowel,2 per constant)",()=>{
+        const word="test";
+        const points = pointsForWord(word);
+        expect(points).toBe(7);
+    })
+    
+})
+describe("isPalindrome",()=>{
+    it("checks if a word is a palindrome and returns double number of points if it is",()=>{
+        const word="racecar"
+        const word1="car"
+        const yeza=isPalindrome(word)
+        const yeba=isPalindrome(word1)
+        expect(yeza).toBe(true)
+        expect(yeba).toBe(false)
+
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
-// Your code here
+export function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      points +=/[aeiou]/.test(char)?1:2;
+    }
+    return points;
+  }
+export function isPalindrome(lala){
+    let points=0;
+    const word=lala.toLowerCase();
+    const isPal= word===word.split('').reverse().join('');
+    return isPal?true:false
+
+}
